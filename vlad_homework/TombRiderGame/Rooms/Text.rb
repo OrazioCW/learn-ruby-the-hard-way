59b75b097a894ec806fd5afdcbd10b0b5128c9cd
vlad321 <vladimir@blinkbox.com>
2014-10-20 11:46:05 +0100

    game push

---
+++ vlad_homework/TombRiderGame/Rooms/Text.rb
@@ -0,0 +1,22 @@
+module Text
+	
+	@text = [
+	"You enter a dark Tomb full of traps.",
+	"In front of you is tree steps corridor leading to a door.",
+	"Which way you take those steps?",
+	"There is a mummy here.",
+	"The mummy has sward.",
+	"This mummy guard another door.",
+	"How are you going to enter that door?",
+	"Which way you take those steps?",
+	"Which way you take those steps?",
+	"Which way you take those steps?",
+	"Which way you take those steps?",
+	"Which way you take those steps?",
+	"Which way you take those steps?"]
+	
+	def Text.dialog(i) 
+		puts text[i]
+	end
+	
+end
